4d6876443fbc65aa3e0b326b4c4f45bd58a9ad20
Sike Dong <dong.si@husky.neu.edu>
2018-09-22 14:23:00 -0400

    Page Objects Change I

--- test/Pages.py
+++ test/Pages.py
@@ -9,17 +9,21 @@ driver = webdriver.Remote(
     command_executor='http://127.0.0.1:4444/wd/hub',
     desired_capabilities=DesiredCapabilities.CHROME)
 
+""" class Basepage(object):
+    def __init__(self,driver):
+        self.driver = driver """
+
 class google_main_page(page):
     def get_main_page(self):
-        driver.get("http://www.google.com")
+        return "http://www.google.com"
     sleep(2)
 
     def search_input(self, keyword):
-        driver.find_element_by_css_selector('.gLFyf.gsfi').send_keys(keyword)
+        search_input = find_element_by_css_selector('.gLFyf.gsfi').send_keys(keyword)
     sleep(3)
 
     def click_search_button(self):
-        driver.find_element_by_css_selector('.gLFyf.gsfi').send_keys(Keys.ENTER)
+        click = find_element_by_css_selector('.gLFyf.gsfi').send_keys(Keys.ENTER)
     sleep(3)
 
     def search(self, keyword):
