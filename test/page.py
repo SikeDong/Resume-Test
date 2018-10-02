a71a98f8b53fd73727ff0a99d8e5dc345974b22a
Sike Dong <dong.si@husky.neu.edu>
2018-10-02 17:25:01 -0400

    Page Objects Login Change V

--- test/page.py
+++ test/page.py
@@ -36,7 +36,7 @@ class google_result_page(Basepage):
 
 class liaoyuan_main_page(Basepage):
     url = 'https://liaoyuan.io/'
-    logo_locator = (By.CSS_SELECTOR, 'div > img[alt = "「燎原」"')
+    logo_locator = (By.CSS_SELECTOR, 'div > img[alt = "「燎原」"]')
     login_button_locator = (By.LINK_TEXT,'登录')
 
     def verify_liaoyuan(self):
@@ -49,34 +49,41 @@ class liaoyuan_main_page(Basepage):
         login = self.driver.find_element(*self.login_button_locator)
         login.click()
         sleep(2)
-
+    
 class login_page(Basepage):
     login_url = 'https://liaoyuan.io/login/'
-    login_username = (By.LINK_TEXT,'邮箱')
-    login_password = (By.LINK_TEXT,'密码')
-    liaoyuan_login_button_locator = (By.LINK_TEXT,'登录')
+    liaoyuan_username = (By.CSS_SELECTOR,'input[type="email"]')
+    liaoyuan_password = (By.CSS_SELECTOR,'input[type="password"]')
+    login_liaoyuan_button = (By.CSS_SELECTOR,'.action-wrapper')
 
-    def verify_login_page(self):
-        print(self.driver.current_url)
-        assert self.driver.current_url == 'https://liaoyuan.io/login/', 'cannot open login page'
+    """def verify_login_page(self):
+        print('current_url:', self.driver.current_url)
+        assert self.driver.current_url == 'https://liaoyuan.io/login/', 'cannot open login page'"""
 
     def login_username(self,username):
-        ##login.username = self.driver.find_element_by_link_text('邮箱').send_keys(username)
-        liaoyuan_login_username = self.wait_locator(self.login_username)
-        liaoyuan_login_username.send_keys(username)
-        assert login.username == "username","不存在该用户名"
+        self.wait_locator(self.liaoyuan_username)
+        login_liaoyuan_username = self.driver.find_element(*self.liaoyuan_username)
+        login_liaoyuan_username.send_keys(username)
         sleep(2)
         
     def login_password(self,password):
-        ##login.password = self.driver.find_element_by_link_text('密码').send_keys(password)
-        liaoyuan_login_password = self.wait_locator(self.login_password)
-        liaoyuan_login_password.send_keys(password)
-        assert login.password =="password", "密码不正确"
+        self.wait_locator(self.liaoyuan_password)
+        login_liaoyuan_password = self.driver.find_element(*self.liaoyuan_password)
+        login_liaoyuan_password.send_keys(password)
         sleep(2)
     
-    def click_login_button(self):
-        self.wait_locator(self.liaoyuan_login_button_locator)
-        login_button_click = self.driver.find_element(*self.liaoyuan_login_button_locator)
-        login_button_click.click()
+    def click_login_liaoyuan(self):
+        self.wait_locator(self.login_liaoyuan_button)
+        login_button = self.driver.find_element(*self.login_liaoyuan_button)
+        print("login button:", login_button)
+        login_button.click()
         sleep(2)
+
+class login_main_page(Basepage):
+    url = "https://liaoyuan.io/profession/"
+    
+    def verify_login_main_page(self):
+        assert self.driver.current_url == 'https://liaoyuan.io/profession/', "cannot login"
+
+
     
--- test/test.py
+++ test/test.py
@@ -20,7 +20,7 @@ class TestLiaoyuan(unittest.TestCase):
         self.driver.close()
         pass
 
-    def test_search_liaoyuan(self):
+    def xtest_search_liaoyuan(self):
         main_page = page.google_main_page(self.driver)
         main_page.search_input("燎原")
         main_page.click_search_button()
@@ -31,14 +31,18 @@ class TestLiaoyuan(unittest.TestCase):
         get_liaoyuan = page.liaoyuan_main_page(self.driver)
         get_liaoyuan.verify_liaoyuan()
         get_liaoyuan.click_login()
-
+    
     def test_liaoyuan_login(self):
+        self.driver.get("https://liaoyuan.io/login/")
         test_login = page.login_page(self.driver)
-        test_login.verify_login_page()
-        test_login.login_username("dsk932015@gmail.com")
+        test_login.login_username("sike@us.liaoyuan.io")
+        ##assert self.login_liaoyuan_username == "username","不存在该用户名"
         test_login.login_password("Dsk930123")
-        test_login.click_login_button()
+        ##assert self.login_liaoyuan_password == "password", "密码不正确"
+        test_login.click_login_liaoyuan()  
 
+        get_login = page.login_main_page(self.driver)
+        get_login.verify_login_main_page()
 
 if __name__ == "__main__":
     unittest.main() # run all tests
